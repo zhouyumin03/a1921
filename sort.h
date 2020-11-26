22132d0dea7848eca0fa89b044d52ae1520bda71
root <root@DESKTOP-CI42T04.localdomain>
2020-11-26 15:55:16 +0800

    first commit

---
+++ sort.h
@@ -0,0 +1,14 @@
+#ifndef sort_h
+#define sort_h
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void Bubble(int a[],int n);
+void Insert(int a[], int n);
+void Select(int a[], int n);
+void Quick(int a[], int n);                                                          
+
+#endif
+
+
